9cfbdaec6d18b6cffbeffb1127d68f78f7f2d5e3
Meir Sedlis <meirsedlis@gmail.com>
2022-08-10 16:01:31 -0500

    create reducers

--- src/features/lookup/lookupSlice.js
+++ src/features/lookup/lookupSlice.js
@@ -1,16 +1,62 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchByCountyPIN } from './lookupAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchByCountyPIN } from "./lookupAPI";
 
 const initialState = {
-    value: 0,
-    status: 'idle',
-  };
+  value: "",
+  status: "idle",
+};
 
-  export const lookupByCountyPIN = createAsyncThunk(
-    'lookup/fetchByCountyPIN',
-    async (PIN) => {
-        // const response = await fetchByCountyPIN(PIN);
-        // return response.data;
-        console.log(PIN)
-    }
-  );
+export const lookupSlice = createSlice({
+  name: "lookup",
+  initialState,
+  reducers: {
+    aNumberPre: (state, action) => {
+      state.value += action.payload;
+    },
+    aNumber: (state, action) => {
+      state.value += action.payload;
+    },
+    aNumberSuf: (state, action) => {
+      state.value += action.payload;
+    },
+    stPreMod: (state, action) => {
+      state.value += action.payload;
+    },
+    stPreDir: (state, action) => {
+      state.value += action.payload;
+    },
+    stPreTyp: (state, action) => {
+      state.value += action.payload;
+    },
+    stPreSep: (state, action) => {
+      state.value += action.payload;
+    },
+    stName: (state, action) => {
+      state.value += action.payload;
+    },
+    stPosTyp: (state, action) => {
+      state.value += action.payload;
+    },
+    stPosDir: (state, action) => {
+      state.value += action.payload;
+    },
+    zip: (state, action) => {
+      state.value += action.payload;
+    },
+    city: (state, action) => {
+      state.value += action.payload;
+    },
+    county: (state, action) => {
+      state.value += action.payload;
+    },
+    stateShort: (state, action) => {
+      state.value += action.payload;
+    },
+    ownerName: (state, action) => {
+      state.value += action.payload;
+    },
+    totalValue: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
